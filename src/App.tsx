4cb7cd307d91781b5f09fba6ece042fa20c870e8
Alexander Melo <alexandermelox@gmail.com>
2021-03-14 19:01:26 -0400

    added colors to themes file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
-import { ChakraProvider, theme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme'
 
 export const App = () => <ChakraProvider theme={theme}>Test</ChakraProvider>
---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+const theme = {
+  colors: {
+    primary: {
+      violet: '#5964E0',
+      lightViolet: '#939BF4',
+      veryDarkBlue: '#19202D',
+      midnight: '#121721',
+    },
+    secondary: {
+      white: '#ffffff',
+      lightGray: '#F4F6F8',
+      gray: '#9DAEC2',
+      darkGray: '#6E8098',
+    },
+  },
+  config: {
+    useSystemColorMode: true,
+  },
+}
+
+export default theme
